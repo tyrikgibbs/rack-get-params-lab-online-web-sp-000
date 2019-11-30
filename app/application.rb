59d169d987d91b2d7fa919315b2b698c04b02af8
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-30 21:03:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,9 +14,14 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    else
-      resp.write "Your cart is empty"
-    end
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |cart_item|
+          resp.write "#{cart_item}\n"
+        end
+      end
 
     resp.finish
   end
