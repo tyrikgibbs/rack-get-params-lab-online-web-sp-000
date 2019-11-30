b8c3bb5a153a1adc96e447b537d0b1a3e7ad944c
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-30 20:54:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,6 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     else
-      resp.write "Path Not Found"
       resp.write "Your cart is empty"
     end
 
